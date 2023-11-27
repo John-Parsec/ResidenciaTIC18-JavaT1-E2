26a511882516f235c9f2a5b0fbd7c1915a32df37
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-11-27 12:13:19 -0300

    feature: classa ListaSessoes

--- redeSocial/src/redeSocial/ListaSessoes.java
+++ redeSocial/src/redeSocial/ListaSessoes.java
@@ -1,5 +1,19 @@
 package redeSocial;
 
+import java.util.List;
+
 public class ListaSessoes {
+    private List<Sessao> listaSessao = new ArrayList<>();
+
+    public void addSessao(Sessao sessao) {
+        listaSessao.add(sessao);
+    }
+    public removeSessao(Sessao sessao) {
+        listaSessao.remove(sessao);
+    }
+    public List<Sessao> getListaSessao() {
+        return listaSessao;
+    }
 
 }
+
