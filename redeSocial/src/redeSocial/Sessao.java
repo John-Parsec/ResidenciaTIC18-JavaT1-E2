afa93c4fdef4a64333df7ab4e76ebfa1cb314719
gustavogonzag <gustavosg91@gmail.com>
2023-11-27 11:45:24 -0300

    feat: Adicionar Remover usuario + Post

--- redeSocial/src/redeSocial/Sessao.java
+++ redeSocial/src/redeSocial/Sessao.java
@@ -11,12 +11,10 @@ public class Sessao {
 		super();
 		this.dataHoraInicio = new Date();
 	}
-	
 
     static void logar(Usuario user){
         Sessao sessao = new Sessao();
         sessao.setUser(user);
-        
         user.setSessao(sessao);
     }
     
@@ -25,6 +23,26 @@ public class Sessao {
     	user.setSessao(null);
     }
 
+	public criarAmizade(Usuario amigo2) {
+		if (!this.user.amigos.contains(amigo2)) {
+            this.user.adcionaAmigo(amigo2); // Adiciona o usuário atual à lista de amigos do amigo
+            amigo2.amigos.adcionaAmigo(this); // Adiciona o usuário atual à lista de amigos do amigo
+        }
+	}
+
+	public desfazerAmizade(Usuario amigo2) {
+		if (this.user.amigos.contains(amigo2)) {
+			this.user.removeAmigo(amigo2); // Remove o usuário atual da lista de amigos do amigo
+			amigo2.amigos.removeAmigo(this); // Remove o usuário atual da lista de amigos do amigo
+		}
+	}
+
+	public criarPostagem(String post){
+		Postagem postagem = new Postagem();
+		postagem.setDataPostagem(new Date());
+		postagem.setPost(this.user.getNome() + " postou: " + post);
+		this.user.novaPostagem(postagem);
+	}
 
 	public Usuario getUser() {
 		return user;
