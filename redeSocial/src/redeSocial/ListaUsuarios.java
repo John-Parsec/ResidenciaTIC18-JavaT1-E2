c266d8ca0bcfba1eb2a3115dc59cbcc296e48027
gustavogonzag <gustavosg91@gmail.com>
2023-11-27 12:04:25 -0300

    feat: lista de usarios

--- redeSocial/src/redeSocial/ListaUsuarios.java
+++ redeSocial/src/redeSocial/ListaUsuarios.java
@@ -1,5 +1,25 @@
 package redeSocial;
 
 public class ListaUsuarios {
+    private List<Usuario> lista = new ArrayList<>();
 
+    public void add(Usuario user) {
+        lista.add(user);
+    }
+
+    public void remove(Usuario user) {
+        lista.remove(user);
+    }
+
+    public List<Usuario> getLista() {
+        return lista;
+    }
+
+    public findUser(Usuario user){
+        if(!lista.contains(user)){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
