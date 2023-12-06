d214b8e2e10557b5dce68d000f5fe59da97c9a1a
Wilton Freire <wilton.lio@gmail.com>
2023-12-06 17:34:30 -0300

    Enviando class Pagamento e Reembolso

---
+++ EmpresaCOELHO/src/Reembolso.java
@@ -0,0 +1,33 @@
+public class Reembolso {
+    private double valor;
+    private Date data;
+
+    public Reembolso(double valor, Date data) {
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Reembolso{" +
+                "valor=" + valor +
+                ", data=" + data +
+                '}';
+    }
+}
